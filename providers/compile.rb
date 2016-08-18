3316235e2253ffb0739dbdedbe6ff35975516c1c
Jim Dowling <jdowling@sics.se>
2016-08-18 23:49:47 +0200

    dont fail if nvidia drivers not found

--- providers/compile.rb
+++ providers/compile.rb
@@ -4,9 +4,6 @@ bash "validate_cuda" do
     user node.tensorflow.user
     code <<-EOF
     set -e
-#    export PATH=$PATH:#{node.cuda.base_dir}
-#    export LD_LIBRARY_PATH=#{node.cuda.base_dir}/lib64:$LD_LIBRARY_PATH
-#    export CUDA_HOME==#{node.cuda.base_dir}
 # test the cuda nvidia compiler
     nvcc -V
 EOF
@@ -21,11 +18,7 @@ bash "validate_cudnn" do
     user "root"
     code <<-EOF
     set -e
-#    export PATH=$PATH:#{node.cuda.base_dir}
-#    export LD_LIBRARY_PATH=#{node.cuda.base_dir}/lib64:$LD_LIBRARY_PATH
-#    export CUDA_HOME==#{node.cuda.base_dir}
-
-    nvidia-smi
+    nvidia-smi | grep NVID
 EOF
 end
 
