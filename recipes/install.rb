ef42f1093f00185dfed1f8e6d6d427cfb1f6f049
Jim Dowling <jdowling@sics.se>
2016-12-01 11:39:53 +0100

    Add python pillow

--- recipes/install.rb
+++ recipes/install.rb
@@ -36,7 +36,7 @@ when "debian"
 
 execute 'apt-get update -y'
 
-  packages = %w{pkg-config zip g++ zlib1g-dev unzip swig git build-essential cmake unzip libopenblas-dev liblapack-dev linux-image-generic linux-image-extra-virtual linux-source linux-headers-generic python python-numpy python-dev python-pip python-lxml }
+  packages = %w{pkg-config zip g++ zlib1g-dev unzip swig git build-essential cmake unzip libopenblas-dev liblapack-dev linux-image-generic linux-image-extra-virtual linux-source linux-headers-generic python python-numpy python-dev python-pip python-lxml python-pillow }
   for script in packages do
     package script do
       action :install
@@ -76,6 +76,9 @@ when "rhel"
     action :install
   end
   package "python27-numpy" do
+    action :install
+  end
+  package "python-pillow" do
     action :install
   end
 
