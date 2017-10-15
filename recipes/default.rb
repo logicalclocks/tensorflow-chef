ed2be1ef3922aa3ef217403c6df466ba88118f08
karamel <jdowling@sics.se>
2017-10-16 01:22:28 +0200

    only install hdfs resources in first tensorflow ip

--- recipes/default.rb
+++ recipes/default.rb
@@ -62,101 +62,109 @@ end
 # conda index /tmp/my-conda-channel/linux-64/                       
 
 
-url=node['tensorflow']['python_url']
-base_filename =  File.basename(url)
-cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
-
-remote_file cached_filename do
-  source url
-  mode 0755
-  action :create
-end
-
-hops_hdfs_directory cached_filename do
-  action :put_as_superuser
-  owner node['hops']['hdfs']['user']
-  group node['hops']['group']
-  mode "1755"
-  dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
-end
+# Only the first NN needs to create the directories
+if private_ip.eql? node['tensorflow']['default']['private_ips'][0]
 
 
-url=node['tensorflow']['tfspark_url']
+  url=node['tensorflow']['python_url']
+  base_filename =  File.basename(url)
+  cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
 
-base_filename =  File.basename(url)
-cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
+  remote_file cached_filename do
+    source url
+    mode 0755
+    action :create
+  end
 
-remote_file cached_filename do
-  source url
-  mode 0755
-  action :create
-end
-
-hops_hdfs_directory cached_filename do
-  action :put_as_superuser
-  owner node['hops']['hdfs']['user']
-  group node['hops']['group']
-  mode "1755"
-  dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
-end
-
-url=node['tensorflow']['hopstf_url']
-
-base_filename =  File.basename(url)
-cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
-
-remote_file cached_filename do
-  source url
-  mode 0755
-  action :create
-end
-
-hops_hdfs_directory cached_filename do
-  action :put_as_superuser
-  owner node['hops']['hdfs']['user']
-  group node['hops']['group']
-  mode "1755"
-  dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
-end
+  hops_hdfs_directory cached_filename do
+    action :put_as_superuser
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    mode "1755"
+    dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
+  end
 
 
+  url=node['tensorflow']['tfspark_url']
 
-url=node['tensorflow']['hopstfdemo_url']
+  base_filename =  File.basename(url)
+  cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
 
-base_filename =  File.basename(url)
-cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
+  remote_file cached_filename do
+    source url
+    mode 0755
+    action :create
+  end
 
-remote_file cached_filename do
-  source url
-  mode 0755
-  action :create
-end
+  hops_hdfs_directory cached_filename do
+    action :put_as_superuser
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    mode "1755"
+    dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
+  end
 
-# Extract mnist
-bash 'extract_mnist' do
-        user "root"
-        code <<-EOH
+  url=node['tensorflow']['hopstf_url']
+
+  base_filename =  File.basename(url)
+  cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
+
+  remote_file cached_filename do
+    source url
+    mode 0755
+    action :create
+  end
+
+  hops_hdfs_directory cached_filename do
+    action :put_as_superuser
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    mode "1755"
+    dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
+  end
+
+
+
+  url=node['tensorflow']['hopstfdemo_url']
+
+  base_filename =  File.basename(url)
+  cached_filename = "#{Chef::Config['file_cache_path']}/#{base_filename}"
+
+  remote_file cached_filename do
+    source url
+    mode 0755
+    action :create
+  end
+
+  # Extract mnist
+  bash 'extract_mnist' do
+    user "root"
+    code <<-EOH
                 set -e
                 tar -zxf #{Chef::Config['file_cache_path']}/#{base_filename} -C #{Chef::Config['file_cache_path']}
                 chown -RL #{node['hops']['hdfs']['user']}:#{node['hops']['group']} #{Chef::Config['file_cache_path']}/#{node['tensorflow']['base_dirname']}
         EOH
-        not_if { ::File.exists?("#{Chef::Config['file_cache_path']}/#{node['tensorflow']['base_dirname']}") }
+    not_if { ::File.exists?("#{Chef::Config['file_cache_path']}/#{node['tensorflow']['base_dirname']}") }
+  end
+
+  hops_hdfs_directory "/user/#{node['hops']['hdfs']['user']}/#{node['tensorflow']['hopstfdemo_dir']}" do
+    action :create_as_superuser
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    mode "1775"
+  end
+
+  hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{node['tensorflow']['base_dirname']}/*" do
+    action :put_as_superuser
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    isDir true
+    mode "1755"
+    dest "/user/#{node['hops']['hdfs']['user']}/#{node['tensorflow']['hopstfdemo_dir']}"
+  end
+
+
 end
 
-hops_hdfs_directory "/user/#{node['hops']['hdfs']['user']}/#{node['tensorflow']['hopstfdemo_dir']}" do
-  action :create_as_superuser
-  owner node['hops']['hdfs']['user']
-  group node['hops']['group']
-  mode "1775"
-end
-
-hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{node['tensorflow']['base_dirname']}/*" do
-  action :put_as_superuser
-  owner node['hops']['hdfs']['user']
-  group node['hops']['group']
-  isDir true
-  mode "1755"
-  dest "/user/#{node['hops']['hdfs']['user']}/#{node['tensorflow']['hopstfdemo_dir']}"
-end
 
 # libibverbs-devel
