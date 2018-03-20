daf3dc8d1724efe85fae884f1234d1c96fa5f1e3
Jim Dowling <jdowling@sics.se>
2018-03-20 10:14:56 +0000

    [HOPSWORKS-468] Kagent restart, performance impact assessment  (#18)
    * dont create tensorflow homedir
    
    * enable inifiband modules for ubuntu
    
    * tensorflow.version is in kagent/attributes/default

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,70 +1,8 @@
 private_ip = my_private_ip()
 
 
-directory node['tensorflow']['home'] do
-  owner node['tensorflow']['user']
-  group node['tensorflow']['group']
-  mode "750"
-  action :create
-end
-
-link node['tensorflow']['base_dir'] do
-  action :delete
-  only_if "test -L #{node['tensorflow']['base_dir']}"
-end
-
-link node['tensorflow']['base_dir'] do
-  owner node['tensorflow']['user']
-  group node['tensorflow']['group']
-  to node['tensorflow']['home']
-end
-
-directory "#{node['tensorflow']['home']}/bin" do
-  owner node['tensorflow']['user']
-  group node['tensorflow']['group']
-  mode "750"
-  action :create
-end
-
-template "#{node['tensorflow']['base_dir']}/bin/launcher" do 
-  source "launcher.sh.erb"
-  owner node['tensorflow']['user']
-  group node['tensorflow']['group']
-  mode "750"
-  # variables({
-  #             :myNN => "hdfs://" + firstNN
-  #           })
-  action :create_if_missing
-end
-
-template "#{node['tensorflow']['base_dir']}/bin/kill-process.sh" do 
-  source "kill-process.sh.erb"
-  owner node['tensorflow']['user']
-  group node['tensorflow']['group']
-  mode "750"
-  action :create_if_missing
-end
-
-
-
-
-directory "#{node['tensorflow']['home']}/hops-channel/linux-64" do
-  owner node['tensorflow']['user']
-  group node['tensorflow']['group']
-  mode "755"
-  recursive true
-  action :create
-  not_if { File.directory?("#{node['tensorflow']['home']}/hops-channel/linux-64") }
-end
-
-
-
-# conda index /tmp/my-conda-channel/linux-64/                       
-
-
-# Only the first NN needs to create the directories
+# Only the first tensorflow server needs to create the directories in HDFS
 if private_ip.eql? node['tensorflow']['default']['private_ips'][0]
-
 
   url=node['tensorflow']['hopstf_url']
 
@@ -84,8 +22,6 @@ if private_ip.eql? node['tensorflow']['default']['private_ips'][0]
     mode "1755"
     dest "/user/#{node['hops']['hdfs']['user']}/#{base_filename}"
   end
-
-
 
   url=node['tensorflow']['hopstfdemo_url']
 
@@ -125,8 +61,5 @@ if private_ip.eql? node['tensorflow']['default']['private_ips'][0]
     dest "/user/#{node['hops']['hdfs']['user']}/#{node['tensorflow']['hopstfdemo_dir']}"
   end
 
-
 end
 
-
-# libibverbs-devel
