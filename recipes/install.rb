0ab2fa4828ac22ff9b2258fb7292cdf7d045aac5
Jim Dowling <jdowling@sics.se>
2018-04-04 07:22:26 +0200

    reformat

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,3 +1,4 @@
+
 group node["tensorflow"]["group"] do
   action :create
   not_if "getent group #{node["tensorflow"]["group"]}"
@@ -38,13 +39,6 @@ link node["tensorflow"]["base_dir"] do
   group node["tensorflow"]["group"]
   to node["tensorflow"]["home"]
 end
-
-
-
-
-
-
-
 
 # First, find out the compute capability of your GPU here: https://developer.nvidia.com/cuda-gpus
 # E.g.,
@@ -103,12 +97,11 @@ if node['tensorflow']['rdma'].eql? "true"
 
     package "libipathverbs-dev"
 
-    
-# Install inifiband
-# https://community.mellanox.com/docs/DOC-2683
-  bash "install-infiniband-ubuntu" do
-    user "root"
-    code <<-EOF
+    # Install inifiband
+    # https://community.mellanox.com/docs/DOC-2683
+    bash "install-infiniband-ubuntu" do
+      user "root"
+      code <<-EOF
     set -e
      apt-get install libmlx4-1 libmlx5-1 ibutils  rdmacm-utils libibverbs1 ibverbs-utils perftest infiniband-diags libibverbs-dev -y
      apt-get -y install libibcm1 libibverbs1 ibverbs-utils librdmacm1 rdmacm-utils libdapl2 ibsim-utils ibutils libcxgb3-1 libibmad5 libibumad3 libmlx4-1 libmthca1 libnes1 infiniband-diags mstflint opensm perftest srptools
@@ -127,16 +120,16 @@ if node['tensorflow']['rdma'].eql? "true"
      sudo modprobe iw_nes
      sudo modprobe iw_c2
     EOF
-  end
+    end
   else # "rhel"
     # http://www.rdmamojo.com/2014/10/11/working-rdma-redhatcentos-7/
     # https://community.mellanox.com/docs/DOC-2086
 
 
-# Get started - check hardware exists
-# [root@hadoop5 install]#  lspci |grep -i infin
-# 03:00.0 InfiniBand: QLogic Corp. IBA7322 QDR InfiniBand HCA (rev 02)
-# [root@hadoop5 install]# lspci -Qvvs 03:00.0
+    # Get started - check hardware exists
+    # [root@hadoop5 install]#  lspci |grep -i infin
+    # 03:00.0 InfiniBand: QLogic Corp. IBA7322 QDR InfiniBand HCA (rev 02)
+    # [root@hadoop5 install]# lspci -Qvvs 03:00.0
     # The last line will tell you what kernel module you need to load. In my case, it was:
     # 	Kernel modules: ib_qib
 
@@ -144,57 +137,57 @@ if node['tensorflow']['rdma'].eql? "true"
     # lsmod | grep ib_
     # Then check it is installed
 
-# [root@hadoop5 install]# ibstat
-# CA 'qib0'
-# 	CA type: InfiniPath_QLE7340
-# 	Number of ports: 1
-# 	Firmware version:
-# 	Hardware version: 2
-# 	Node GUID: 0x001175000076dcbe
-# 	System image GUID: 0x001175000076dcbe
-# 	Port 1:
-# 		State: Active
-# 		Physical state: LinkUp
-# 		Rate: 40
-# 		Base lid: 6
-# 		LMC: 0
-# 		SM lid: 3
-# 		Capability mask: 0x07610868
-# 		Port GUID: 0x001175000076dcbe
-# 		Link layer: InfiniBand
+    # [root@hadoop5 install]# ibstat
+    # CA 'qib0'
+    # 	CA type: InfiniPath_QLE7340
+    # 	Number of ports: 1
+    # 	Firmware version:
+    # 	Hardware version: 2
+    # 	Node GUID: 0x001175000076dcbe
+    # 	System image GUID: 0x001175000076dcbe
+    # 	Port 1:
+    # 		State: Active
+    # 		Physical state: LinkUp
+    # 		Rate: 40
+    # 		Base lid: 6
+    # 		LMC: 0
+    # 		SM lid: 3
+    # 		Capability mask: 0x07610868
+    # 		Port GUID: 0x001175000076dcbe
+    # 		Link layer: InfiniBand
 
     # To measure bandwith, on the server run: 'ib_send_bw'
     # On the client, 'ib_send_bw hadoop5'
-#      ib_read_bw
-# ---------------------------------------------------------------------------------------
-# Device not recognized to implement inline feature. Disabling it
+    #      ib_read_bw
+    # ---------------------------------------------------------------------------------------
+    # Device not recognized to implement inline feature. Disabling it
 
-# ************************************
-# * Waiting for client to connect... *
-# ************************************
-# ---------------------------------------------------------------------------------------
-#                     RDMA_Read BW Test
-#  Dual-port       : OFF		Device         : qib0
-#  Number of qps   : 1		Transport type : IB
-#  Connection type : RC		Using SRQ      : OFF
-#  CQ Moderation   : 100
-#  Mtu             : 2048[B]
-#  Link type       : IB
-#  Outstand reads  : 16
-#  rdma_cm QPs	 : OFF
-#  Data ex. method : Ethernet
-# ---------------------------------------------------------------------------------------
-#  local address: LID 0x06 QPN 0x000b PSN 0x2045ef OUT 0x10 RKey 0x030400 VAddr 0x007f9d36566000
-#  remote address: LID 0x03 QPN 0x0013 PSN 0x8c947f OUT 0x10 RKey 0x070800 VAddr 0x007ff8638c7000
-# ---------------------------------------------------------------------------------------
-#  #bytes     #iterations    BW peak[MB/sec]    BW average[MB/sec]   MsgRate[Mpps]
-#  65536      1000             2563.33            2563.29		   0.041013
-# ---------------------------------------------------------------------------------------
+    # ************************************
+    # * Waiting for client to connect... *
+    # ************************************
+    # ---------------------------------------------------------------------------------------
+    #                     RDMA_Read BW Test
+    #  Dual-port       : OFF		Device         : qib0
+    #  Number of qps   : 1		Transport type : IB
+    #  Connection type : RC		Using SRQ      : OFF
+    #  CQ Moderation   : 100
+    #  Mtu             : 2048[B]
+    #  Link type       : IB
+    #  Outstand reads  : 16
+    #  rdma_cm QPs	 : OFF
+    #  Data ex. method : Ethernet
+    # ---------------------------------------------------------------------------------------
+    #  local address: LID 0x06 QPN 0x000b PSN 0x2045ef OUT 0x10 RKey 0x030400 VAddr 0x007f9d36566000
+    #  remote address: LID 0x03 QPN 0x0013 PSN 0x8c947f OUT 0x10 RKey 0x070800 VAddr 0x007ff8638c7000
+    # ---------------------------------------------------------------------------------------
+    #  #bytes     #iterations    BW peak[MB/sec]    BW average[MB/sec]   MsgRate[Mpps]
+    #  65536      1000             2563.33            2563.29		   0.041013
+    # ---------------------------------------------------------------------------------------
 
 
-  bash "install-infiniband-rhel" do
-    user "root"
-    code <<-EOF
+    bash "install-infiniband-rhel" do
+      user "root"
+      code <<-EOF
     set -e
     yum -y groupinstall "Infiniband Support"
     yum --setopt=group_package_types=optional groupinstall "Infiniband Support" -y
@@ -206,32 +199,11 @@ if node['tensorflow']['rdma'].eql? "true"
 #    modprobe mlx4_ib
 #    modprobe mlx5_ib
    EOF
-    not_if "systemctl status rdma"
+      not_if "systemctl status rdma"
+    end
   end
-  end
 end
 
-
-group node['tensorflow']['group'] do
-  action :create
-  not_if "getent group #{node['tensorflow']['group']}"
-end
-
-user node['tensorflow']['user'] do
-  action :create
-  gid node['tensorflow']['group']
-  manage_home true
-  home "/home/#{node['tensorflow']['user']}"
-  shell "/bin/bash"
-  not_if "getent passwd #{node['tensorflow']['user']}"
-end
-
-group node['tensorflow']['group'] do
-  action :modify
-  members ["#{node['tensorflow']['user']}"]
-  append true
-  not_if "getent passwd #{node['tensorflow']['user']}"
-end
 
 # http://www.pyimagesearch.com/2016/07/04/how-to-install-cuda-toolkit-and-cudnn-for-deep-learning/
 case node['platform_family']
@@ -270,19 +242,19 @@ EOF
   end
 
   # https://negativo17.org/nvidia-driver/
- # nvidia_packages = %w{ nvidia-driver nvidia-driver-libs.x86_64 dkms-nvidia cuda-devel cuda-libs cuda-cudnn cuda-cudnn-devel cuda-cli-tools cuda-cupti-devel cuda-extra-libs }
- #  for driver in nvidia_packages do
- #    package driver do
- #      action :install
- #    end
- #  end
+  # nvidia_packages = %w{ nvidia-driver nvidia-driver-libs.x86_64 dkms-nvidia cuda-devel cuda-libs cuda-cudnn cuda-cudnn-devel cuda-cli-tools cuda-cupti-devel cuda-extra-libs }
+  #  for driver in nvidia_packages do
+  #    package driver do
+  #      action :install
+  #    end
+  #  end
 
 
 end
 
 bash "pip-upgrade" do
-    user "root"
-    code <<-EOF
+  user "root"
+  code <<-EOF
     set -e
     pip install --upgrade pip
     EOF
@@ -507,23 +479,23 @@ if node['tensorflow']['mpi'] == "true"
     package "libopenmpi-dev"
     package "mpi-default-bin"
 
-#     bash "install-nccl2-ubuntu" do
-#       user "root"
-#       code <<-EOF
-# #       set -e
-#        cd #{Chef::Config['file_cache_path']}
-#        rm -f nccl-repo-ubuntu1604-2.0.5-ga-cuda8.0_2-1_amd64.deb
-#        wget http://snurran.sics.se/hops/nccl-repo-ubuntu1604-2.0.5-ga-cuda8.0_2-1_amd64.deb
-#        dpkg -i nccl-repo-ubuntu1604-2.0.5-ga-cuda8.0_2-1_amd64.deb
-#        apt-key add /var/nccl-repo-2.0.5-ga-cuda8.0/7fa2af80.pub
-#        apt update
-#        apt install libnccl2 libnccl-dev
+  #     bash "install-nccl2-ubuntu" do
+  #       user "root"
+  #       code <<-EOF
+  # #       set -e
+  #        cd #{Chef::Config['file_cache_path']}
+  #        rm -f nccl-repo-ubuntu1604-2.0.5-ga-cuda8.0_2-1_amd64.deb
+  #        wget http://snurran.sics.se/hops/nccl-repo-ubuntu1604-2.0.5-ga-cuda8.0_2-1_amd64.deb
+  #        dpkg -i nccl-repo-ubuntu1604-2.0.5-ga-cuda8.0_2-1_amd64.deb
+  #        apt-key add /var/nccl-repo-2.0.5-ga-cuda8.0/7fa2af80.pub
+  #        apt update
+  #        apt install libnccl2 libnccl-dev
 
-#        # https://github.com/uber/horovod/blob/master/docs/gpus.md
-#        # HOROVOD_GPU_ALLGATHER=MPI HOROVOD_GPU_BROADCAST=MPI HOROVOD_GPU_ALLREDUCE=NCCL pip install --no-cache-dir horovod
-#        # HOROVOD_GPU_ALLREDUCE=MPI HOROVOD_GPU_ALLGATHER=MPI HOROVOD_GPU_BROADCAST=MPI pip install --no-cache-dir horovod
-#     EOF
-#     end
+  #        # https://github.com/uber/horovod/blob/master/docs/gpus.md
+  #        # HOROVOD_GPU_ALLGATHER=MPI HOROVOD_GPU_BROADCAST=MPI HOROVOD_GPU_ALLREDUCE=NCCL pip install --no-cache-dir horovod
+  #        # HOROVOD_GPU_ALLREDUCE=MPI HOROVOD_GPU_ALLGATHER=MPI HOROVOD_GPU_BROADCAST=MPI pip install --no-cache-dir horovod
+  #     EOF
+  #     end
 
   when "rhel"
     # installs binaries to /usr/local/bin
@@ -535,15 +507,15 @@ if node['tensorflow']['mpi'] == "true"
     end    
   end
 
-    magic_shell_environment 'LD_LIBRARY_PATH' do
-      value "$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/nccl2/lib"
-    end
+  magic_shell_environment 'LD_LIBRARY_PATH' do
+    value "$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/nccl2/lib"
+  end
   
 
-    nccl2=node['cuda']['nccl_version']
-    bash "install-nccl2" do
-      user "root"
-      code <<-EOF
+  nccl2=node['cuda']['nccl_version']
+  bash "install-nccl2" do
+    user "root"
+    code <<-EOF
        set -e
        cd #{Chef::Config['file_cache_path']}
        rm -f #{nccl2}.txz
@@ -557,16 +529,16 @@ if node['tensorflow']['mpi'] == "true"
        rm -f /usr/local/nccl2
        ln -s /usr/local/#{nccl2} /usr/local/nccl2
     EOF
-      not_if { File.directory?("/usr/local/#{nccl2}") }
-    end
+    not_if { File.directory?("/usr/local/#{nccl2}") }
+  end
   
 end
 
 if node['tensorflow']['install'].eql?("src")
 
-    # https://wiki.fysik.dtu.dk/niflheim/OmniPath#openmpi-configuration
-    # compile openmpi on centos 7
-    # https://bitsanddragons.wordpress.com/2017/05/08/install-openmpi-2-1-0-on-centos-7/
+  # https://wiki.fysik.dtu.dk/niflheim/OmniPath#openmpi-configuration
+  # compile openmpi on centos 7
+  # https://bitsanddragons.wordpress.com/2017/05/08/install-openmpi-2-1-0-on-centos-7/
 
   tensorflow_compile "mpi-compile" do
     action :openmpi
