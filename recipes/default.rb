2336714ca4909204dccb32d80ab2a5468809f1f2
Jim Dowling <jdowling@sics.se>
2018-04-12 22:35:59 +0200

    create environments here

--- recipes/default.rb
+++ recipes/default.rb
@@ -63,3 +63,68 @@ if private_ip.eql? node['tensorflow']['default']['private_ips'][0]
 
 end
 
+
+python_versions = %w{ 2.7 3.6 }
+for python in python_versions
+
+  bash "conda_py#{python}_env" do
+    user node['conda']['user']
+    group node['conda']['group']
+    code <<-EOF
+
+    export CONDA_DIR=#{node['conda']['base_dir']}
+    export PY=$(echo #{python} | sed 's/\\.//')
+    export PROJECT=python${PY}
+    export MPI=#{node['tensorflow']['need_mpi']}
+
+    ${CONDA_DIR}/bin/conda info --envs | grep "^python${PY}"
+    if [ $? -eq 0 ] ; then 
+       exit 0
+    fi
+
+    ${CONDA_DIR}/bin/conda create -n $PROJECT python=#{python} -y -q
+
+
+    export HADOOP_HOME=#{node['kagent']['dir']}/hadoop
+
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --pre --upgrade pydoop
+
+    if [ "$PY" == "27" ] ; then
+        yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade tensorflow-serving-api
+    fi
+
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade hopsfacets
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade tfspark
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade ipykernel
+
+    # Install a custom build of tensorflow with this line.
+    ##{node['conda']['base_dir']}/envs/${PROJECT}/bin/pip install --upgrade #{node['conda']['base_dir']}/pkgs/tensorflow${GPU}-#{node['tensorflow']['version']}-cp${PY}-cp${PY}mu-manylinux1_x86_64.whl"
+
+
+    # If cuda is installed, and there is a GPU, install TF with GPUs
+    GPU=
+    if [ -f /usr/local/cuda/version.txt ]  ; then
+        nvidia-smi -L | grep -i gpu
+    	if [ $? -eq 0 ] ; then
+            GPU="-gpu"
+            yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip uninstall tensorflow
+    	    if [ $? -ne 0 ] ; then
+                echo "Problem uninstalling tensorflow to prepare for gpu version"
+            fi
+        fi
+    fi
+
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install tensorflow${GPU}==#{node['tensorflow']['version']}  --upgrade --force-reinstall
+    
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade hops
+
+    if [ "${MPI}" == "true" ] ; then
+       yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade horovod
+    fi
+
+    yes | ${CONDA_DIR}/envs/${PROJECT}/bin/pip install --upgrade #{node['mml']['url']}
+
+ EOF
+  end
+
+end
