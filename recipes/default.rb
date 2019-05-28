9d42f5cc98066c2962a27616d937c2c026d7aac0
Kim Hammar <kimham@kth.se>
2019-05-28 14:55:16 +0200

    rebase fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -278,7 +278,7 @@ for python in python_versions
 # jupyter nbextension enable --py --sys-prefix witwidget
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade witwidget
     if [ $? -ne 0 ] ; then
-       exit 10
+       exit 11
     fi
 
 
@@ -324,7 +324,7 @@ for python in python_versions
       fi
     fi
     if [ $? -ne 0 ] ; then
-       exit 11
+       exit 12
     fi
 
     export HOPS_UTIL_PY_VERSION=#{node['conda']['hops-util-py']['version']}
@@ -337,27 +337,27 @@ for python in python_versions
         yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install hops==$HOPS_UTIL_PY_VERSION
     fi
     if [ $? -ne 0 ] ; then
-       exit 12
+       exit 13
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade pyjks
     if [ $? -ne 0 ] ; then
-       exit 13
+       exit 14
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade confluent-kafka
     if [ $? -ne 0 ] ; then
-       exit 14
+       exit 15
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade hops-petastorm
     if [ $? -ne 0 ] ; then
-       exit 15
+       exit 16
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade opencv-python
     if [ $? -ne 0 ] ; then
-       exit 16
+       exit 17
     fi
 
     export PYTORCH_CHANNEL=#{node['conda']['channels']['pytorch']}
@@ -367,12 +367,12 @@ for python in python_versions
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install maggy==#{node['maggy']['version']}
     if [ $? -ne 0 ] ; then
-      exit 4
+      exit 18
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade tqdm
     if [ $? -ne 0 ] ; then
-       exit 15
+       exit 19
     fi
 
 
@@ -380,46 +380,46 @@ for python in python_versions
       if [ "#{python}" == "2.7" ] ; then
         ${CONDA_DIR}/bin/conda install -y -n ${ENV} -c ${PYTORCH_CHANNEL} pytorch=#{node['pytorch']['version']}=#{node["pytorch"]["python2"]["build"]} torchvision=#{node['torchvision']['version']} cudatoolkit=#{node['cudatoolkit']['version']}
         if [ $? -ne 0 ] ; then
-          exit 17
+          exit 20
         fi
       else
         ${CONDA_DIR}/bin/conda install -y -n ${ENV} -c ${PYTORCH_CHANNEL} pytorch=#{node['pytorch']['version']}=#{node["pytorch"]["python3"]["build"]} torchvision=#{node['torchvision']['version']} cudatoolkit=#{node['cudatoolkit']['version']}
         if [ $? -ne 0 ] ; then
-          exit 18
+          exit 21
         fi
       fi
       ${CONDA_DIR}/bin/conda remove -y -n ${ENV} cudatoolkit=#{node['cudatoolkit']['version']} --force
       if [ $? -ne 0 ] ; then
-        exit 19
+        exit 22
       fi
     else
       ${CONDA_DIR}/bin/conda install -y -n ${ENV} -c ${PYTORCH_CHANNEL} pytorch-cpu=#{node['pytorch']['version']} torchvision-cpu=#{node['torchvision']['version']}
       if [ $? -ne 0 ] ; then
-        exit 20
+        exit 23
       fi
     fi
 
     # This is a temporary fix for pytorch 1.0.1 https://github.com/pytorch/pytorch/issues/16775
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install typing
     if [ $? -ne 0 ] ; then
-       exit 21
+       exit 24
     fi
 
     # for sklearn serving
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade Flask
     if [ $? -ne 0 ] ; then
-       exit 22
+       exit 25
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade scikit-learn
     if [ $? -ne 0 ] ; then
-       exit 23
+       exit 26
     fi
 
     yes | ${CONDA_DIR}/envs/${ENV}/bin/pip install --upgrade avro
     if [ $? -ne 0 ] ; then
-       exit 24
+       exit 27
     fi
 
     EOF
