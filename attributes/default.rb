7bcdbbabca68ee0c59fba9d084cd07a3a4c2ecb8
Jim Dowling <jdowling@sics.se>
2019-06-23 14:46:26 +0200

    upgrade maggy

--- attributes/default.rb
+++ attributes/default.rb
@@ -140,4 +140,4 @@ default['featurestore']['hops_featurestore_demo_url'] = "#{node['download_url']}
 
 # Maggy - dist optimization for TensorFlow/Spark
 #
-default['maggy']['version']                           = "0.1.1"
+default['maggy']['version']                           = "0.2.2"
