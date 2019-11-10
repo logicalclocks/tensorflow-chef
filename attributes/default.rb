cfe584cb06763335f19ac36467d777cc06a54192
Jim Dowling <jdowling@sics.se>
2019-11-10 23:05:11 +0100

    pytorch 1.3.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,7 +12,7 @@ default['tensorflow']['rocm']['version']         = "1.14.0"
 
 default["tensorflow"]['serving']["version"]      = "1.14.0"
 default["cudatoolkit"]["version"]                = "10.0"
-default["pytorch"]["version"]                    = "1.3.0"
+default["pytorch"]["version"]                    = "1.3.1"
 default["pytorch"]["python2"]["build"]           = "py2.7_cuda10.0.130_cudnn7.6.3_0"
 default["pytorch"]["python3"]["build"]           = "py3.6_cuda10.0.130_cudnn7.6.3_0"
 default["torchvision"]["version"]                = "0.4.2"
