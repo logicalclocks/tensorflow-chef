07f15c2468e72a2e15ecceb6642abada5900a636
robzor <robin.eric.andersson@gmail.com>
2020-02-10 13:23:53 +0100

    [HOPSWORKS-1344] Remove tensorflow compile code from tensorflow-chef (#102)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,6 @@
 include_attribute "conda"
 include_attribute "kagent"
 
-default['tensorflow']['user']          = node['tensorflow'].attribute?('user') ? node['install']['user'] : node['kagent']['user']
-default['tensorflow']['group']         = node['install']['user'].empty? ? node['kagent']['group'] : node['install']['user']
-
 # tensorflow and tensorflow-gpu version
 default["tensorflow"]["version"]                 = "1.14.0"
 
@@ -24,13 +21,9 @@ default["numpy"]["version"]                      = "1.16.5"
 default['pyspark']['version']                    = "2.4.3"
 default['tfx']['version']                        = "0.14.0"
 
-default['tensorflow']['install']       = "dist" # or 'src' or 'custom'
-
 # tensorflow-1.2.1-debian-gcc_version-python_version.whl
 # #{node['download_url']}/tensorflow-#{node['tensorflow']['version']}-#{node['platform']}-5.4-2.7.whl"
 default['tensorflow']['custom_url']    = ""
-
-default['tensorflow']['git_url']       = "https://github.com/tensorflow/tensorflow"
 
 #
 # TensorFlow/PyTorch example notebooks and datasets
@@ -38,13 +31,6 @@ default['tensorflow']['git_url']       = "https://github.com/tensorflow/tensorfl
 default['tensorflow']['examples_version']  = node['install']['version']
 default['tensorflow']['hopstfdemo_dir'] = "tensorflow_demo"
 default['tensorflow']['hopstfdemo_url'] = "#{node['download_url']}/tensorflow/#{node['tensorflow']['examples_version']}/demo.tar.gz"
-
-#
-# Base directories
-#
-default['tensorflow']['dir']           = node['install']['dir'].empty? ? "/srv/hops" : node['install']['dir']
-default['tensorflow']['home']          = node['tensorflow']['dir'] + "/tensorflow-" + node['tensorflow']['version']
-default['tensorflow']['base_dir']      = node['tensorflow']['dir'] + "/tensorflow"
 
 # Comma separated list of supported cuda versions (~ # of patches )
 default['cuda']['versions']            = "9.0.176_384.81~2,10.0.130_410.48~1"
@@ -72,11 +58,6 @@ default['cudnn']['base_url']                = "#{node['download_url']}/cudnn"
 default['nccl']['version_mapping']          = "2.2.13-1+9.0,2.3.5-2+10.0"
 default['nccl']['base_url']         = "#{node['download_url']}/nccl"
 
-# TensorRT - Nvidia (ubuntu only)
-# TensorRT-3.0.4.Ubuntu-16.04.3.x86_64.cuda-9.0.cudnn7.0.tar.gz
-default['cuda']['tensorrt']            = "3.0.4"
-default['cuda']['tensorrt_version']    = "TensorRT-#{node['cuda']['tensorrt']}.Ubuntu-16.04.3.x86_64-gnu.cuda-9.0.cudnn7.0.tar.gz"
-
 #
 # AMD - ROCm dist found at http://repo.radeon.com/rocm/
 #
@@ -103,25 +84,11 @@ default['rocm']['dir']           = node['install']['dir'].empty? ? "/srv/hops" :
 default['rocm']['base_dir']      = node['rocm']['dir'] + "/rocm"
 
 default['tensorflow']['mkl']           = "false"
-default['tensorflow']['mpi']           = "false"
 default['tensorflow']['rdma']          = "false"
-default['tensorflow']['tensorrt']      = "false"
-
 
 default['tensorflow']['need_cuda']     = 0
-default['tensorflow']['need_mpi']      = 0
 default['tensorflow']['need_mkl']      = 0
 default['tensorflow']['need_rdma']     = 0
-default['tensorflow']['need_tensorrt'] = 0
-
-# https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-installer-linux-x86_64.sh
-default['bazel']['major_version']      = "0.11"
-default['bazel']['minor_version']      = "1"
-default['bazel']['version']            = node['bazel']['major_version'] + "." + node['bazel']['minor_version']
-default['bazel']['url']                = "#{node['download_url']}/bazel-#{node['bazel']['version']}-installer-linux-x86_64.sh"
-
-default['openmpi']['version']          = "openmpi-3.1.0.tar.gz"
-
 
 default['jupyter']['sparkmagic']['version']            = "0.12.8"
 default['jupyter']['sparkmagic']['url']                = node['download_url'] + "/sparkmagic-" + node['jupyter']['sparkmagic']['version'] + ".tar.gz"
@@ -131,13 +98,11 @@ default['python2']['ipykernel_version']                = "4.10.0"
 default['python2']['jupyter_console_version']          = "5.2.0"
 default['python2']['ipython_version']                  = "5.8.0"
 
-
 # Feature Store example notebooks and datasets
 #
 default['featurestore']['examples_version']           = node['install']['version']
 default['featurestore']['hops_featurestore_demo_dir'] = "featurestore_demo"
 default['featurestore']['hops_featurestore_demo_url'] = "#{node['download_url']}/featurestore/#{node['featurestore']['examples_version']}/featurestore.tar.gz"
-
 
 # Maggy - dist optimization for TensorFlow/Spark
 #
