ebb10637346b668377a7a6388b04e6ec503af043
Robin Andersson <robin.eric.andersson@gmail.com>
2020-02-27 10:54:35 +0100

    [HOPSWORKS-1644] Install tensorflow model server from hosted .deb file instead of apt-get

--- recipes/serving.rb
+++ recipes/serving.rb
@@ -6,20 +6,22 @@ when "debian"
     libpng_pkg_name = "libpng12-dev"
   end
 
-  package ["build-essential","curl","libcurl3-dev","git","libfreetype6-dev", libpng_pkg_name,"libzmq3-dev","pkg-config","python-dev","python-numpy","python-pip","software-properties-common","swig","zip","zlib1g-dev"]
+  package ["build-essential","curl","libcurl3-dev","git","libfreetype6-dev", libpng_pkg_name,"libzmq3-dev","pkg-config","python-dev","python-numpy","python-pip","software-properties-common","swig","zip","zlib1g-dev","libstdc++6"]
 
-  bash 'prepare_tf_serving' do
+  # Download serving server
+  model_server_dpkg = "#{Chef::Config['file_cache_path']}/tensorflow-model-server_#{node['tensorflow']['serving']['version']}_all.deb"
+  remote_file model_server_dpkg do
     user "root"
-    #ignore_failure true
-    code <<-EOF
-      echo "deb [arch=amd64] http://storage.googleapis.com/tensorflow-serving-apt testing tensorflow-model-server tensorflow-model-server-#{node['tensorflow']['serving']['version']}" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list
-      curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -        
-      add-apt-repository ppa:ubuntu-toolchain-r/test -y
-      apt-get update
-    EOF
+    group "root"
+    source node['tensorflow']['serving']['url']
+    mode 0755
+    action :create
   end
 
-  package ["libstdc++6", "tensorflow-model-server"]
+  dpkg_package "tensorflow-model-server" do
+    source model_server_dpkg
+    action :install
+  end
 
 when "rhel"
   package ["curl", "libcurl", "git", "freetype-devel", "libpng12-devel", "python2-pkgconfig", "python-devel", "python2-pip", "swig", "zip", "zlib-devel", "giflib-devel", "zeromq3-devel"]
