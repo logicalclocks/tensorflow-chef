c4f9733d36988c317ca698355616f3f88254374d
robzor92 <robin.eric.andersson@gmail.com>
2022-07-08 23:44:53 +0200

    bump driver version

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@ default['cuda']['skip_test']           = "false"
 default['cuda']['skip_stop_xserver']   = "false"
 
 # Nvidia driver
-default['nvidia']['driver_version']      = "450.102.04"
+default['nvidia']['driver_version']      = "515.48.07"
 default['nvidia']['driver_url']          = "#{node['download_url']}/NVIDIA-Linux-x86_64-#{node['nvidia']['driver_version']}.run"
 #
 # AMD - ROCm dist found at http://repo.radeon.com/rocm/
