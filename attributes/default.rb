ffcb0ba1ad874d915af15ffaffa225ceb7f55a30
Fabio Buso <buso.fabio@gmail.com>
2022-12-05 00:04:35 +0100

    [FSTORE-505] Remove old feature store tour files

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,9 +18,3 @@ default['cuda']['skip_stop_xserver']   = "false"
 # Nvidia driver
 default['nvidia']['driver_version']      = "515.48.07"
 default['nvidia']['driver_url']          = "#{node['download_url']}/NVIDIA-Linux-x86_64-#{node['nvidia']['driver_version']}.run"
-
-# Feature Store example notebooks and datasets
-#
-default['featurestore']['examples_version']           = node['install']['version']
-default['featurestore']['hops_featurestore_demo_dir'] = "featurestore_demo"
-default['featurestore']['hops_featurestore_demo_url'] = "#{node['download_url']}/featurestore/#{node['featurestore']['examples_version']}/featurestore.tar.gz"
