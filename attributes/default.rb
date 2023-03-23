021accdaeea687ede4cd512fd3dd95678184f159
robzor92 <robin.eric.andersson@gmail.com>
2023-03-23 21:06:15 +0100

    [HWORKS-470] Bump NVIDIA driver version to support CUDA 11.8

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,5 +16,5 @@ default['cuda']['skip_test']           = "false"
 default['cuda']['skip_stop_xserver']   = "false"
 
 # Nvidia driver
-default['nvidia']['driver_version']      = "515.48.07"
+default['nvidia']['driver_version']      = "525.85.12"
 default['nvidia']['driver_url']          = "#{node['download_url']}/NVIDIA-Linux-x86_64-#{node['nvidia']['driver_version']}.run"
