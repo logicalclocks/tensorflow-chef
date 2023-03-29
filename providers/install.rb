4be1ae675c41b17c051fc522dd2ac9459675e74a
robzor92 <robin.eric.andersson@gmail.com>
2023-03-29 12:49:38 +0200

    [HWORKS-505] Upgrade GPU components to support Cgroups v2/Rhel8/Ubuntu22.04

--- providers/install.rb
+++ providers/install.rb
@@ -63,10 +63,12 @@ action :driver do
       end
     end
 
-    package ['kernel-headers', 'libglvnd-glx', 'dkms', 'rpm-build', 'redhat-rpm-config', 'asciidoc', 'hmaccalc', 'perl-ExtUtils-Embed', 'pesign', 'xmlto', 'bison', 'bc', 'audit-libs-devel', 'binutils-devel', 'elfutils-devel', 'elfutils-libelf-devel', 'ncurses-devel', 'newt-devel', 'numactl-devel', 'pciutils-devel', 'python-devel', 'zlib-devel'] do
+    # from https://docs.nvidia.com/ai-enterprise/deployment-guide-bare-metal/0.1.0/first-system.html
+    package ['kernel-headers', 'kernel-devel', 'dkms', 'tar', 'bzip2', 'make', 'automake', 'gcc', 'gcc-c++', 'pciutils', 'libglvnd-devel'] do
       retries 10
       retry_delay 30
     end
+
 
     bash "install_driver_centos" do
       user "root"
@@ -74,18 +76,10 @@ action :driver do
       cwd Chef::Config['file_cache_path']
       code <<-EOF
         set -e
-        # https://devtalk.nvidia.com/default/topic/1012901/unable-to-install-driver-375-66-on-centos-7/?offset=5
-        # There seems to be a non-standard installation path for the kernel sources in Centos
-        # The 'ks=...' tries to resolve the directory where they should be installed inside /lib/modules/...
-        # ks=$(rpm -qa | grep kernel | head -1 | sed -e 's/kernel-//' | sed -e 's/\.x86_64//')
-        # ksl=$(rpm -qa | grep kernel | head -1 | sed -e 's/kernel-//')
-        # --kernel-source-path==#{::Dir.home(node['kagent']['user'])}/rpmbuild/BUILD/kernel-${ks}/linux-${ksl}/
-        #
-        # rm -f /lib/modules/3.10.0-514.el7.x86_64/build
-        # cd /lib/modules/3.10.0-514.el7.x86_64/build
-        # ln -s /usr/src/kernels/3.10.0-693.21.1.el7.x86_64/ build
-        #  --kernel-source-path=/lib/modules/3.10.0-514.el7.x86_64/build
-        #
+        # The NVIDIA driver requires that the kernel headers and development packages for the running
+        # version of the kernel be installed at the time of the driver installation, as well whenever
+        # the driver is rebuilt. For example, if your system is running kernel version 4.4.0, the 4.4.0
+        # kernel headers and development packages must also be installed.
         ./#{driver} -a --install-libglvnd --force-libglx-indirect -q --dkms --compat32-libdir -s --ui=none --no-cc-version-check
       EOF
       only_if  "[ \"$(modinfo nvidia | grep \"^version:\" | awk '{split($0,a,\" \"); print a[2]}')\" != \"#{new_resource.driver_version}\" ]"
