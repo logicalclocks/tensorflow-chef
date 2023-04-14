7a0f9d19ab335d802d60d30e39e047b61e03b09f
Fabio Buso <buso.fabio@gmail.com>
2023-04-14 18:14:07 +0200

    [HWORKS-455] Remove old tours (#159)

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,13 +4,6 @@ include_attribute "kagent"
 default["tensorflow"]["version"]                 = "2.11.0"
 default['tensorflow']['serving']['version']      = "2.11.0"
 
-#
-# TensorFlow/PyTorch example notebooks and datasets
-#
-default['tensorflow']['examples_version']  = node['install']['version']
-default['tensorflow']['hopstfdemo_dir'] = "tensorflow_demo"
-default['tensorflow']['hopstfdemo_url'] = "#{node['download_url']}/tensorflow/#{node['tensorflow']['examples_version']}/demo.tar.gz"
-
 default['cuda']['accept_nvidia_download_terms']        = "false"
 default['cuda']['skip_test']           = "false"
 default['cuda']['skip_stop_xserver']   = "false"
